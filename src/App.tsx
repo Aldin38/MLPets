bea29ba7cf6d9d9ed6381a08a0d998150ea5d6b8
Aldin Mikara <aldin.mikara@oryxgaming.com>
2022-11-18 18:09:12 +0100

    setup basic canvas for three js

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './assets/styles/styles.scss';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Card } from './components/Card';
+
 
 function App() {
   return (
-    <div>Pola cijelo</div>
+    <div className='game-screen'>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Card position={[-1.2, 0, 0]} />
+        <Card position={[1.2, 0, 0]} />
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 }
 
