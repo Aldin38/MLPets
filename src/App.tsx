25dbf3ebea256808c07622e46b0095b65bc7d95b
Aldin Mikara <aldin.mikara@gmail.com>
2023-12-06 20:44:33 +0100

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import './assets/styles/styles.scss';
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { Card } from './components/Card';
-
+import "./assets/styles/styles.scss";
 
 function App() {
-  return (
-    <div className='game-screen'>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Card position={[-1.2, 0, 0]} />
-        <Card position={[1.2, 0, 0]} />
-        <OrbitControls />
-      </Canvas>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
